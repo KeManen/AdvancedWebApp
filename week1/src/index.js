cdeee0460040f226f757007478f864061611b483
KeMa <keskimattinen.konsta@gmail.com>
2022-11-02 23:38:11 +0200

    full points

--- week1/src/index.js
+++ week1/src/index.js
@@ -1,37 +1,45 @@
-if(document.readyState !== "loading") init()
-else document.addEventListener("DOMContentLoaded", init)
+import "./styles.css";
 
-
-function init (){
-	fetch("https://dog.ceo/api/breeds/list")
-		.then((res) => res.json())
-		.then(async (data)=>{
-			let dogs = data.message.splice(0,5)
-			for (let breed of dogs) {
-				let image = await fetch(`https://dog.ceo/api/breed/${breed}/images/random`).then((res)=>res.json());
-				let wikiJson = await fetch(`https://en.wikipedia.org/api/rest_v1/page/summary/${breed}`).then((res)=> res.json());
-				createWikiElement(breed, wikiJson['extract'], image.message);
-			}
-		}).catch(err => console.log(err));
+if (document.readyState !== "loading") {
+	initializeCode();
+} else {
+	document.addEventListener("DOMContentLoaded", function () {
+		initializeCode();
+	});
 }
 
-function createWikiElement(headerText, wikiText, imageSrc){
+async function initializeCode() {
+	let dogs = ["affenpinscher", "african", "airedale", "akita", "appenzeller"];
+	for (let breed of dogs) {
+		let image = await fetch(`https://dog.ceo/api/breed/${breed}/images/random`)
+			.then((res) => res.json())
+			.catch((err) => console.log(err));
+		let wikiJson = await fetch(
+			`https://en.wikipedia.org/api/rest_v1/page/summary/${breed}`
+		)
+			.then((res) => res.json())
+			.catch((err) => console.log(err));
+		createWikiElement(breed, wikiJson["extract"], image.message);
+	}
+}
+
+function createWikiElement(headerText, wikiText, imageSrc) {
 	let item = document.createElement("div");
 	let header = document.createElement("h1");
 	let content = document.createElement("div");
 	let text = document.createElement("p");
 	let imgcontainer = document.createElement("div");
 	let img = document.createElement("img");
-	
+
 	item.className = "wiki-item";
-	header.className="wiki-header";
-	content.className="wiki-content";
-	text.className="wiki-text";
-	imgcontainer.className="img-container";
-	img.className="wiki-img";
-	
-	header.innerHTML = headerText;
-	text.innerHTML = wikiText;
+	header.className = "wiki-header";
+	content.className = "wiki-content";
+	text.className = "wiki-text";
+	imgcontainer.className = "img-container";
+	img.className = "wiki-img";
+
+	header.innerText = headerText;
+	text.innerText = wikiText;
 	img.src = imageSrc;
 
 	item.appendChild(header);
