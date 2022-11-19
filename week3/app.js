e366a79585a06136a2eac9dad8036307926bbae4
KeMa <keskimattinen.konsta@gmail.com>
2022-11-19 17:11:16 +0200

    week3 finally working

--- week3/app.js
+++ week3/app.js
@@ -5,13 +5,12 @@ let cookieParser = require('cookie-parser');
 let logger = require('morgan');
 
 let indexRouter = require('./routes/index');
-let usersRouter = require('./routes/user');
 
 let app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'pug');
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -20,7 +19,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/user/', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- week3/routes/index.js
+++ week3/routes/index.js
@@ -1,14 +1,17 @@
 let express = require('express');
 let router = express.Router();
-let users = require('../models/data');
+let users = {};
 
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { users: users});
+router.get('/', function(req, res) {
+  res.render('index', );
 });
 
 router.post('/todo', (req, res) => {
+  console.log(req.body)
+  console.log(req.body.name)
+  console.log(req.body.task)
   let isNewUser = users[req.body.name] == null
   let user = isNewUser ? { name: req.body.name, tasks:[]} : users[req.body.name];
   user.tasks.push(req.body.task);
@@ -16,5 +19,34 @@ router.post('/todo', (req, res) => {
   if(isNewUser) res.send("User added");
   else res.send("Todo added");
 })
+
+/* GET users listing. */
+router.get('/user/:id', function(req, res) {
+  console.log(req.params.id)
+  let user = users[req.params.id];
+  console.log(users)
+  console.log(user)
+  if(user == null) res.send({"error":"User not found"});
+  else res.send({"user":user});
+});
+
+router.delete('/user/:id', (req, res) => {
+  if(users[req.params.id] == null) return res.send("User not found");
+  delete(users[req.params.id]);
+  res.send("User deleted")
+});
+
+router.put('/user/', (req, res) => {
+  let user = users[req.body.name];
+  console.log(user)
+  if(user != null){
+    user.tasks= user.tasks.filter(item => item !== req.body.todo)
+    console.log(user)
+    res.send("Task deleted")
+    return;
+  }
+  res.send("User not found");
+});
+
 
 module.exports = router;
